134dfba4f508a82bbf8f1f94cd74c2bfa5ab7638
Robiyaxon <tuhtaevarobiahon@gmail.com>
2024-12-26 14:34:33 +0500

    fixed error

---
+++ src/Companent/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Button.css"
+const Button = () => {
+  return (
+    <div>
+        <button className='btn'>Hover me</button>
+    </div>
+  )
+}
+
+//hello
+//hello
+export default Button
